1bb446c3a4813a9114eeeccf339bd1df0413c4d8
mcelrjo <mcelroy@auburn.edu>
2025-05-07 20:44:23 -0500

    Trying to fix newsfeed

--- src/App.jsx
+++ src/App.jsx
@@ -99,10 +99,18 @@ function App() {
           <h2>Lawn Care News</h2>
           <div style="position: relative; width: 100%; padding-top: 177.78%; /* Aspect ratio: 1600/900 * 100 */">
             <iframe
-                src="https://rss.app/embed/v1/feed/Y2w4zPxrObmELkuA"
-                frameBorder="0"
-                style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;">
-            </iframe>
+              src="https://rss.app/embed/v1/feed/Y2w4zPxrObmELkuA"
+              frameBorder="0"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                border: 0
+              }}
+              title="Lawn Care News"
+            />
           </div>
         </aside>
       </div>
