c0dd521374e3ce84d090621927313cdad6686007
mcelrjo <mcelroy@auburn.edu>
2025-05-11 14:17:00 -0500

    trying again to add news page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ChatPage from "./ChatPage";
-import './App.css'
+import LawnCareNews from "./LawnCareNews";
+import './App.css';
 import logo from "./assets/AIGreenLogo.png";
 
 function App() {
   return (
-    <div>
+    <Router>
       <header className="header-banner">
         <img src={logo} alt="LawnCare Assistant Logo" className="logo" />
         <div style={{ flexGrow: 1 }}>
@@ -13,26 +15,31 @@ function App() {
             powered by Landscape World Network
           </p>
         </div>
-        <a
-          href="/news"
-          style={{
-            color: "white",
-            textDecoration: "none",
-            marginRight: "1rem",
-            backgroundColor: "#3E7E3C",
-            padding: "0.3rem 0.7rem",
-            borderRadius: "5px"
-          }}
-        >
-          LawnCare News
-        </a>
+        <nav>
+          <Link
+            to="/news"
+            style={{
+              color: "white",
+              textDecoration: "none",
+              marginRight: "1rem",
+              backgroundColor: "#3E7E3C",
+              padding: "0.3rem 0.7rem",
+              borderRadius: "5px"
+            }}
+          >
+            LawnCare News
+          </Link>
+        </nav>
       </header>
 
-      {/* Directly render ChatPage without Router or Routes */}
-      <ChatPage />
-    </div>
+      <Routes>
+        <Route path="/" element={<ChatPage />} />
+        <Route path="/news" element={<LawnCareNews />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
 
